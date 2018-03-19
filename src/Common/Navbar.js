e47161fea513f15233c2df1ada56e2ebb4a64aad
siddharth sharma <sandesh251189@gmail.com>
2018-03-19 07:25:24 -0400

    Add Routes and pages

--- src/Common/Navbar.js
+++ src/Common/Navbar.js
@@ -1,10 +1,30 @@
 import React , {Component} from "react" ;
 import {Link } from 'react-router-dom';
 const Navbar=()=>(
-    <ul>
-      <li><Link to='/'>Home</Link></li>
-      <li><Link to='/Signup'>Sign Up</Link></li>
-    </ul>
+
+
+  <nav class="navbar navbar-inverse navbar-fixed-top">
+    <div class="container">
+      <div class="navbar-header">
+        <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+          <span class="sr-only">Toggle navigation</span>
+          <span class="icon-bar"></span>
+          <span class="icon-bar"></span>
+          <span class="icon-bar"></span>
+        </button>
+        <a class="navbar-brand" href="#">Bootstrap theme</a>
+      </div>
+      <div id="navbar" class="navbar-collapse collapse">
+        <ul class="nav navbar-nav">
+          <li class="active"><Link to="/">Home</Link></li>
+          <li><Link to="/about">About</Link></li>
+          <li><Link to="/Signup">Signup</Link></li>
+
+        </ul>
+      </div>
+    </div>
+  </nav>
+
 )
 
 export default Navbar;
--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/css/bootstrap-theme.min.css';
+import 'bootstrap/dist/js/bootstrap.min.js';
 import './index.css';
 import Signup from './Register/Register';
 import Routers from './Routes/React.Routes';
