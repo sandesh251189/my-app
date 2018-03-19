c6fa9130eee538e4e9d12e08da02c54da1807236
siddharth sharma <sandesh251189@gmail.com>
2018-03-19 02:49:28 -0400

    Install React Router

---
+++ src/Register/Register.js
@@ -0,0 +1,10 @@
+import React , {Component} from "react" ;
+class Register extends Component {
+  render(){
+    return(
+      <h1>This is Signup</h1>
+    )
+  }
+}
+
+export default Register
--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import Signup from './Register/Register';
 import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
